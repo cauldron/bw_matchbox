fd7021bef40d0e10400faf71c1aa9fc5e8ad2693
lilliputten <lilliputten@yandex.ru>
2023-07-30 17:52:26 +0700

    Removed unused installation tricks (we can use `pip install -e .` instead).

--- setup-from-setup-cfg.py
+++
@@ -1,5 +0,0 @@
-# NOTE: Use `setup-from-setup-cfg.sh` to install the dependencies from the `setup.cfg` file.
-import configparser
-c = configparser.ConfigParser()
-c.read('setup.cfg')
-print(c['options']['install_requires'])
--- setup-from-setup-cfg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo " Installing the dependencies from the 'setup.cfg:install_requires' (using 'setup-from-setup-cfg')..."
-python setup-from-setup-cfg.py | xargs pip install
-echo "Done"
