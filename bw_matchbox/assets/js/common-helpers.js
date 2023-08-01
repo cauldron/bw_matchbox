501906a23d3df0e937d7e97876a7a1bdf90d6d9a
lilliputten <lilliputten@yandex.ru>
2023-08-01 20:32:46 +0700

    Added helper utilities for deal with url query parameters string.

--- bw_matchbox/assets/js/common-helpers.js
+++ bw_matchbox/assets/js/common-helpers.js
@@ -43,4 +43,90 @@ const commonHelpers = {
     template.innerHTML = html;
     return template.content.firstChild;
   },
+
+  /** decodeQuery
+   * @param {string | string[]} qs
+   * @param {string} sep
+   * @param {string} eq
+   * @param {any} options
+   * @returns {{}}
+   */
+  decodeQuery(qs, sep, eq, options) {
+    sep = sep || '&';
+    eq = eq || '=';
+    const obj = {};
+    if (typeof qs !== 'string' || qs.length === 0) {
+      return obj;
+    }
+    const regexp = /\+/g;
+    qs = qs.split(sep);
+    let maxKeys = 1000;
+    if (options && typeof options.maxKeys === 'number') {
+      maxKeys = options.maxKeys;
+    }
+    let len = qs.length;
+    // maxKeys <= 0 means that we should not limit keys count
+    if (maxKeys > 0 && len > maxKeys) {
+      len = maxKeys;
+    }
+    for (let i = 0; i < len; ++i) {
+      const x = qs[i].replace(regexp, '%20'),
+        idx = x.indexOf(eq);
+      let kstr, vstr;
+      if (idx >= 0) {
+        kstr = x.substring(0, idx);
+        vstr = x.substring(idx + 1);
+      } else {
+        kstr = x;
+        vstr = '';
+      }
+      const k = decodeURIComponent(kstr);
+      const v = decodeURIComponent(vstr);
+      // if (!hasOwnProperty(obj, k)) {
+      if (!Object.prototype.hasOwnProperty.call(obj, k)) {
+        obj[k] = v;
+      } else if (Array.isArray(obj[k])) {
+        obj[k].push(v);
+      } else {
+        obj[k] = [obj[k], v];
+      }
+    }
+    return obj;
+  },
+
+  /** parseQuery -- Parse url query string (in form `?xx=yy&...` or `xx=yy&...`)
+   * @param {string} search  - Query string
+   * @return {Record<string, string>} query - Query object
+   */
+  parseQuery(search) {
+    if (!search) {
+      return {};
+    }
+    if (search.indexOf('?') === 0) {
+      search = search.substring(1);
+    }
+    return commonHelpers.decodeQuery(search);
+  },
+
+  /** makeQuery
+   * @param {Record<string, string | number | boolean>} params
+   * @param {{ addQuestionSymbol?: boolean; useEmptyValues?: boolean }} opts
+   * @returns {string}
+   */
+  makeQuery(params, opts = {}) {
+    let url = Object.entries(params)
+      .map(([id, val]) => {
+        const valStr = String(val);
+        if ((val == undefined || valStr === '') && !opts.useEmptyValues) {
+          return undefined;
+        }
+        return encodeURI(id) + '=' + encodeURI(String(val));
+      })
+      .filter(Boolean)
+      .join('&');
+    if (opts.addQuestionSymbol && url) {
+      url = '?' + url;
+    }
+    return url;
+  },
 };
