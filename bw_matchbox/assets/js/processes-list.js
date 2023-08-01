3331f9f0f5c30c96cb1f2686d27518a0abfeadf2
lilliputten <lilliputten@yandex.ru>
2023-08-01 13:06:23 +0700

    Issue #5: Added dedicated modules (css, js), cleaned some code in `compare.css`.

---
+++ bw_matchbox/assets/js/processes-list.js
@@ -0,0 +1,28 @@
+/* --global commonHelpers */
+
+/* Process list table client code.
+ */
+
+// global module variable
+// eslint-disable-next-line no-unused-vars
+const processesList = {
+  /** Start entrypoint */
+  start() {
+    const search_bar = document.getElementById('query_string');
+    search_bar.focus();
+
+    const do_search = function () {
+      const qs = encodeURIComponent(document.getElementById('query_string').value);
+      if (qs) {
+        location.assign("{{ url_for('search') }}?database={{ database }}&q=" + qs);
+      }
+    };
+
+    search_bar.addEventListener('click', do_search);
+    search_bar.addEventListener('keypress', function (event) {
+      if (event.key === 'Enter') {
+        do_search();
+      }
+    });
+  },
+};
