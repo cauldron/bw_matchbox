a85ec6cb3818ece16b75bb9dd4d0e09a654f54d0
lilliputten <lilliputten@yandex.ru>
2023-08-28 00:13:12 +0700

    Issue #99: Refactored `Match` as es6 module.

---
+++ bw_matchbox/assets/modules/Match/@types/TMatchData.d.ts
@@ -0,0 +1,7 @@
+interface TMatchData {
+  id: number; // 4604
+  location: string; // 'United States'
+  name: string; // 'Home furniture - Cabinets and non-wood, nonupholstered; at manufacturer'
+  referenceProduct?: string; // ???
+  unit: string; // ''
+}
---
+++ bw_matchbox/assets/modules/Match/@types/TMatchProcess.d.ts
@@ -0,0 +1,6 @@
+interface TMatchProcess {
+  comment: string; // ''
+  location: string; // 'United States'
+  name: string; // 'Home furniture - Cabinets and non-wood, nonupholstered; at manufacturer'
+  unit: string; // 'USD'
+}
